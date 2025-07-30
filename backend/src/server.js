23fb0332049dc6630439c880e9a993eef156cb0c
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-07-30 23:29:46 +0530

    Signup endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
