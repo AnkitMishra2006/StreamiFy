faff34b39d99604d528048433333a35f9a2d35b8
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-08-02 19:32:13 +0530

    Deployment Ready

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api",
+  baseURL: BASE_URL,
   withCredentials: true, //send cookies with requests
 });
